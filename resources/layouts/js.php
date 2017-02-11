f646107eb820e5ba8abd56afa8d9cfcebd086db7
Allard Kévin <contact@allard-kevin.fr>
2017-02-11 19:57:14 +0100

    Suppression fichiers js non utilisé

--- resources/layouts/js.php
+++ resources/layouts/js.php
@@ -1,5 +1,3 @@
 <script src="./node_modules/jquery/dist/jquery.min.js"></script>
 <script src="./node_modules/tether/dist/js/tether.min.js"></script>
 <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="./node_modules/mailcheck/src/mailcheck.min.js "></script>
-<script src="./assets/javascript/main.js"></script>
