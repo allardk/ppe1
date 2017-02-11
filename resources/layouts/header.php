643dae3e6f6190a69672eb0372bd5ae75540959c
Allard Kévin <contact@allard-kevin.fr>
2017-02-11 19:56:45 +0100

    Déclaration css fonts

--- resources/layouts/header.php
+++ resources/layouts/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Le volant Basséen</title>
     <link href="./assets/css/app.css" rel="stylesheet">
+    <link href="./assets/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
 <?php include('nav.php'); ?>
