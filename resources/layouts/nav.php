dd11d80af1272f498b42eae99021726c229f5f6b
Allard Kévin <contact@allard-kevin.fr>
2017-02-12 16:27:29 +0100

    Definition des id afin d'effectuer la verification jQuery + desactivation du bouton de soumission pour la fonction jQuery

--- resources/layouts/nav.php
+++ resources/layouts/nav.php
@@ -29,9 +29,9 @@
             </li>
         </ul>
 
-        <div class="hidden-xs-down hidden-sm-down hidden-md-down form-inline mt-2 mt-md-0">
+        <div id="newsletter" class="hidden-xs-down hidden-sm-down hidden-md-down form-inline mt-2 mt-md-0">
             <input id="email" name="email" required class="form-control mr-sm-2" type="email" placeholder="Newsletter">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLive">
+            <button type="button" class="btn btn-primary" disabled="true" data-toggle="modal" data-target="#exampleModalLive">
                 Subscribe
             </button>
         </div>
