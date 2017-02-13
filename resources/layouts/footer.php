10778d6d67231d84199918e8aef3c54cd83fa0e5
Allard Kévin <contact@allard-kevin.fr>
2017-02-13 10:26:47 +0100

    Ajout lien vers sitemap

--- resources/layouts/footer.php
+++ resources/layouts/footer.php
@@ -3,7 +3,7 @@
   -->
 <footer>
     <hr/>
-    <p>&copy; <?php echo date("Y"); ?> - Le volant Basséen - <a href="#">Site map</a></p>
+    <p>&copy; <?php echo date("Y"); ?> - Le volant Basséen - <a href="sitemap.php">Site map</a></p>
 </footer>
 <!-- /-->
 <?php include('js.php'); ?>
